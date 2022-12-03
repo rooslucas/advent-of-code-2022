70444d006ee5552c67461a11e4612f6960825d40
rooslucas <rooslucas@live.nl>
2022-12-03 20:04:13 +0100

    Implement sum function

--- day_2/day_2.py
+++ day_2/day_2.py
@@ -4,15 +4,16 @@ advent_input.close()
 advent_data = [x.rstrip() for x in a_list]
 
 # Part 1
-win = len([i for i in advent_data if (i == 'A Y')
-          or (i == 'B Z') or (i == 'C X')]) * 6
-draw = len([i for i in advent_data if (i == 'A X')
-           or (i == 'B Y') or (i == 'C Z')]) * 3
-loss = len([i for i in advent_data if (i == 'A Z')
-           or (i == 'B X') or (i == 'C Y')]) * 0
-rock = len([i for i in advent_data if 'X' in i]) * 1
-paper = len([i for i in advent_data if 'Y' in i]) * 2
-scissors = len([i for i in advent_data if 'Z' in i]) * 3
+win = sum([6 for i in advent_data if (i == 'A Y')
+          or (i == 'B Z') or (i == 'C X')])
+draw = sum([3 for i in advent_data if (i == 'A X')
+           or (i == 'B Y') or (i == 'C Z')])
+loss = sum([0 for i in advent_data if (i == 'A Z')
+           or (i == 'B X') or (i == 'C Y')])
+
+rock = sum([1 for i in advent_data if 'X' in i])
+paper = sum([2 for i in advent_data if 'Y' in i])
+scissors = sum([3 for i in advent_data if 'Z' in i])
 
 print(win+draw+loss+scissors+paper+rock)
 
@@ -22,12 +23,12 @@ lose = [i for i in advent_data if 'X' in i]
 draw = [i for i in advent_data if 'Y' in i]
 win = [i for i in advent_data if 'Z' in i]
 
-rocks = (len([i for i in lose if 'B' in i]) +
-         len([i for i in draw if 'A' in i]) + len([i for i in win if 'C' in i])) * 1
-papers = (len([i for i in lose if 'C' in i]) +
-          len([i for i in draw if 'B' in i]) + len([i for i in win if 'A' in i])) * 2
-scissorss = (len([i for i in lose if 'A' in i]) +
-             len([i for i in draw if 'C' in i]) + len([i for i in win if 'B' in i])) * 3
+rocks = sum([1 for i in lose if ('B' in i)]) + \
+    sum([1 for i in draw if 'A' in i]) + sum([1 for i in win if 'C' in i])
+papers = sum([2 for i in lose if 'C' in i]) + \
+    sum([2 for i in draw if 'B' in i]) + len([2 for i in win if 'A' in i])
+scissorss = sum([3 for i in lose if 'A' in i]) + \
+    sum([3 for i in draw if 'C' in i]) + sum([3 for i in win if 'B' in i])
 
 print(rocks + papers + scissorss + (len(lose)*0) +
       (len(draw) * 3) + (len(win) * 6))
